1d9cefbebb15a66f4428d2e98b1f353dec86f7b7
Cagnow <36724003+Cagnow@users.noreply.github.com>
2024-07-01 16:03:26 +0200

    Release

---
+++ Seecrack/Seecrack/Program.cs
@@ -0,0 +1,149 @@
+﻿using Memory;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Channels;
+using System.Text;
+class Program
+{
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool CloseHandle(IntPtr hObject);
+    [DllImport("kernel32.dll")]
+    public static extern int SuspendThread(IntPtr hThread);
+    [DllImport("kernel32.dll")]
+    public static extern int ResumeThread(IntPtr hThread);
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr OpenThread(uint dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+
+    public const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+    public const int PROCESS_QUERY_INFORMATION = 0x0400;
+    public const int MEM_COMMIT = 0x00001000;
+    public const int PAGE_READWRITE = 0x04;
+    public const int PROCESS_WM_READ = 0x0010;
+    public const int THREAD_SUSPEND_RESUME = 0x0002;
+    public Mem MemLib = new Mem();
+  
+
+    static async Task Main(string[] args)
+    {
+        string executablePath = Assembly.GetExecutingAssembly().Location;
+        string executableDirectory = Path.GetDirectoryName(executablePath);
+        string configFilePath = $"{executableDirectory}\\config.ini";
+        Dictionary<string, string> configParameters = ReadConfigFile(configFilePath);
+
+
+        string processPath = configParameters.ContainsKey("processPath") ? configParameters["processPath"] : null;
+        int waitingTime = configParameters.ContainsKey("waitingTime") ? int.Parse(configParameters["waitingTime"]) : 0;
+        string key = configParameters.ContainsKey("key") ? configParameters["key"] : null;
+
+        byte[] keyHex = Encoding.UTF8.GetBytes(key);
+
+        // Convertir le tableau de bytes en chaîne hexadécimale
+        string hex = BitConverter.ToString(keyHex).Replace("-", "").ToUpper();
+        string Serial = "";
+        for (int i = 0; i < hex.Length; i += 2)
+        {
+            // Extraire une paire de caractères (ou moins si à la fin)
+            string paire = hex.Substring(i, Math.Min(2, hex.Length - i));
+            Serial += paire + " ";
+        }
+        Serial = Serial.Trim();
+        Mem memLib = new Mem();
+
+        // Démarrer le processus
+       ProcessStartInfo startInfo = new ProcessStartInfo(processPath);
+       Process targetProcess = Process.Start(startInfo);
+        Thread.Sleep(waitingTime);
+        int pid = memLib.GetProcIdFromName("See");
+        // Attendre que le processus soit prêt
+
+        foreach (ProcessThread thread in targetProcess.Threads)
+        {
+            IntPtr threadHandle = OpenThread(THREAD_SUSPEND_RESUME, false, (uint)thread.Id);
+            if (threadHandle != IntPtr.Zero)
+            {
+                SuspendThread(threadHandle);
+                CloseHandle(threadHandle);
+                Console.WriteLine($"{threadHandle}suspendu");
+            }
+   
+        }
+       
+        memLib.OpenProcess("See");
+
+        // AoB scan and store it in AoBScanResults. We specify our start and end address regions to decrease scan time.
+        //IEnumerable<long> AoBScanResults = await memLib.AoBScan("4D 33 4B 4D 46 34 39 33 34 57 32 33 35 39 31 31 41 37 4E 42", true, true);
+        IEnumerable<long> AoBScanResults = await memLib.AoBScan(Serial, true, true);
+
+        // get the first found address, store it in the variable SingleAoBScanResult
+        long SingleAoBScanResult = AoBScanResults.FirstOrDefault();
+
+            // pop up message box that shows our first result
+
+            // Ex: iterate through each found address. This prints each address in the debug console in Visual Studio.
+            foreach (long res in AoBScanResults)
+            {
+            Console.WriteLine($"{res}");
+            byte[] bytes = BitConverter.GetBytes(res);
+            string addressmod = res.ToString("X");
+            memLib.WriteMemory($"0x{addressmod}", "bytes", "0x35 0x37 0x37 0x39 0x36 0x34 0x35 0x53 0x32 0x53 0x56 0x35 0x35 0x39 0x51 0x51 0x41 0x37 0x4E 0x42");
+            }
+        foreach (ProcessThread thread in targetProcess.Threads)
+        {
+            IntPtr threadHandle = OpenThread(THREAD_SUSPEND_RESUME, false, (uint)thread.Id);
+            if (threadHandle != IntPtr.Zero)
+            {
+                ResumeThread(threadHandle);
+                CloseHandle(threadHandle);
+                Console.WriteLine($"{threadHandle}repris");
+            }
+
+        }
+
+
+    }
+    static Dictionary<string, string> ReadConfigFile(string filePath)
+    {
+        var configParameters = new Dictionary<string, string>();
+
+        try
+        {
+            // Lire chaque ligne du fichier
+            foreach (var line in File.ReadLines(filePath))
+            {
+                // Ignorer les lignes vides ou les commentaires (commençant par #)
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                // Séparer la ligne en clé et valeur
+                var parts = line.Split(new[] { '=' }, 2);
+                if (parts.Length == 2)
+                {
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+
+                    // Ajouter le paramètre au dictionnaire
+                    configParameters[key] = value;
+                }
+                else
+                {
+                    Console.WriteLine($"Ligne invalide dans le fichier de configuration: {line}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // Gérer les erreurs de lecture de fichier
+            Console.WriteLine($"Une erreur s'est produite lors de la lecture du fichier de configuration : {ex.Message}");
+        }
+
+        return configParameters;
+    }
+}
---
+++ Seecrack/readme.txt
@@ -0,0 +1,24 @@
+ ____  ____  _____  _____ ____    _  _      ____  _     ____  _____  ____  _  _____ ____ 
+/  _ \/  __\/__ __\/  __//   _\  / \/ \  /|/  _ \/ \ /\/ ___\/__ __\/  __\/ \/  __// ___\
+| / \||  \/|  / \  |  \  |  /    | || |\ ||| | \|| | |||    \  / \  |  \/|| ||  \  |    \
+| \_/||  __/  | |  |  /_ |  \__  | || | \||| |_/|| \_/|\___ |  | |  |    /| ||  /_ \___ |
+\____/\_/     \_/  \____\\____/  \_/\_/  \|\____/\____/\____/  \_/  \_/\_\\_/\____\\____/
+                                                                                         
+--------------------------------------------------------------------------------------------
+Crack de SEE Electrical V8R3
+
+1- Ouvrir le Fichier config.ini (Ne pas modifier le nom ou l'extension, ou encore le nom des Keys du fichier !!!!)
+	1.1-ProcessPath : Chemin d'accès vers l'exécutable de See Electrical (Par défaut : C:\Program Files (x86)\IGE+XAO\SEE Electrical V8R3\SEE.exe)
+	1.2-WaitingTime : Durée en ms avant la suspension du processus. Ce paramètre peut-être sujet à modification en fonction de la performance du PC sur lequel le crack est exécuté ( Par défaut 300)
+	1-3-Key : Paramètre à changer impérativement lors de la première execution. Il faut récupérer le License ID du PC actuel en lançant une première fois See Electrical > i(en haut à droite)> Enregistrer votre logiciel>Suivant.
+
+2-Enregistrer le fichier .ini
+3-Executer le programme afin de lancer See Electrical à chaque fois
+
+
+
+
+
+
+V1.0-----------
+Release
